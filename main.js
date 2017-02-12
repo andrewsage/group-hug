e810b7d34f5ff83fa264e40e37c49b4620dc63b0
Andrew Sage <andrew@asae.co.uk>
2017-02-12 16:16:21 +0000

    http front end added

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ var DeviceMessage = require("./device_message.js");
 
 console.log("Hugs 1.1.3");
 
+var pjson = require('./package.json');
+console.log(pjson.name + ' : ' + pjson.version);
+
 // add any UPM requires that you need
 // and the rest of your app goes here
 // see the samples for more detailed examples
@@ -43,17 +46,76 @@ var led8 = new mraa.Gpio(9);
 const EV_DRIVER_OFF = 1;
 const EV_DRIVER_ON = 0;
 
+var relayStates = [];
 var activeHugs = [];
 var messageQueue = [];
 
+const STATUS_STARTING = 0;
+const STATUS_TEST_CYCLE = 1;
+const STATUS_QUEUE_ACTIVE = 2;
+
 var leds = [led1, led2, led3, led4, led5, led6, led7, led8];
 var numberLeds = leds.length;
+
+var status = STATUS_STARTING;
 
 turnOffAllLights();
 testCycleLights();
 //turnOnAllLights();
 
 startProcessQueue();
+
+server();
+
+// Starts the built-in web server for the JSON data
+// from the stored barometer readings
+function server() {
+    var app = require("express")();
+    app.get('/', function (req, res) {
+        res.send('<html><head><title>Group Hug</title><meta http-equiv="refresh" content="5"></head><body><center><h1>Group Hug</h1><p>Connected devices: ' + connectedDevices + '</p><p>Active hugs: (' + activeHugs.length + ') ' + activeHugs + '</p><p>Relay states: ' + relayStates + '</p><p><a href="all_off">All Off</a></p><h2>Override</h2><p><a href="hugs1">1 hug</a> | <a href="hugs2">2 hugs</a> | <a href="hugs3">3 hugs</a> | <a href="hugs4">4 hugs</a> | <a href="hugs5">5 hugs</a> | <a href="hugs6">6 hugs</a> | <a href="hugs7">7 hugs</a> | <a href="hugs8">8 hugs</a></center></body></html>');
+    });
+
+    app.get("/hugs", function(req, res) { res.json(activeHugs); });
+    app.get("/status", function(req, res) { res.json(status); });
+    app.get("/devices", function(req, res) { res.json(connectedDevices);});
+    app.get("/all_off", function(req, res) {
+        turnOffAllLights();
+        res.redirect('/');
+    });
+    app.get("/hugs1", function(req, res) {
+        enableLights(1);
+        res.redirect('/');
+    });
+    app.get("/hugs2", function(req, res) {
+        enableLights(2);
+        res.redirect('/');
+    });
+    app.get("/hugs3", function(req, res) {
+        enableLights(3);
+        res.redirect('/');
+    });
+    app.get("/hugs4", function(req, res) {
+        enableLights(4);
+        res.redirect('/');
+    });
+    app.get("/hugs5", function(req, res) {
+        enableLights(5);
+        res.redirect('/');
+    });
+    app.get("/hugs6", function(req, res) {
+        enableLights(6);
+        res.redirect('/');
+    });
+    app.get("/hugs7", function(req, res) {
+        enableLights(7);
+        res.redirect('/');
+    });
+    app.get("/hugs8", function(req, res) {
+        enableLights(8);
+        res.redirect('/');
+    });
+    app.listen(3000);
+}
 
 var standardServiceUUID = "03b80e5aede84b33a7516ce34ec4c700";
 var standardButtonCharacteristicUUID = "7772e5db38684112a1a9f2669d106bf3";
@@ -173,15 +235,17 @@ function enableLights(hugs) {
     for (var i = 0; i < numberLeds; i++) {
         var led = leds[i];
         if (hugs >= i + 1) {
+            relayStates[i] = EV_DRIVER_ON;
             led.write(EV_DRIVER_ON);
         } else {
+            relayStates[i] = EV_DRIVER_OFF;
             led.write(EV_DRIVER_OFF);
         }
     }
 }
 
 function testCycleLights() {
-
+    status = STATUS_TEST_CYCLE;
     var testHugs = 0;
     var periodicActivity = function() {
         enableLights(testHugs);
@@ -198,7 +262,9 @@ function testCycleLights() {
 }
 
 function turnOffAllLights() {
+    activeHugs = [];
     for (var i = 0; i < numberLeds; i++) {
+        relayStates[i] = EV_DRIVER_OFF;
         var led = leds[i];
         led.dir(mraa.DIR_OUT);
         led.write(EV_DRIVER_OFF);
@@ -207,6 +273,7 @@ function turnOffAllLights() {
 
 function turnOnAllLights() {
     for (var i = 0; i < numberLeds; i++) {
+        relayStates[i] = EV_DRIVER_ON;
         var led = leds[i];
         led.dir(mraa.DIR_OUT);
         led.write(EV_DRIVER_ON);
@@ -214,6 +281,7 @@ function turnOnAllLights() {
 }
 
 function startProcessQueue() {
+    status = STATUS_QUEUE_ACTIVE;
     var processQueue = function() {
         
         if(messageQueue.length > 0) {
